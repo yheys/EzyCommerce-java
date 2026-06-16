f844fa59709002f2bbf656a74e12d6b7328561bc
Yilkal Kebede <yheyskebede2015@gmail.com>
2026-06-16 15:43:27 +0300

    Initial commit - User.java added

---
+++ src/ezycommerce/models/User.java
@@ -0,0 +1,54 @@
+package ezycommerce.models;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class User {
+
+    
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private static int totalUsers = 0;
+
+    
+    public User() {    //  Default Constructor
+        totalUsers++;
+    }
+
+    
+    public User(int id, String name, String email, String password) {
+        this.id       = id;
+        this.name     = name;
+        this.email    = email;
+        this.password = password;
+        totalUsers++;
+    }
+
+
+    public int getId()            { return id; }
+    public String getName()       { return name; }
+    public String getEmail()      { return email; }
+    public String getPassword()   { return password; }
+    public static int getTotalUsers() { return totalUsers; }
+
+    
+    public void setId(int id)             { this.id = id; }
+    public void setName(String name)      { this.name = name; }
+    public void setEmail(String email)    { this.email = email; }
+    public void setPassword(String pass)  { this.password = pass; }
+
+    
+    public void displayInfo() {
+        System.out.println("ID    : " + id);
+        System.out.println("Name  : " + name);
+        System.out.println("Email : " + email);
+    }
+
+    
+    @Override
+    public String toString() {
+        return "User[id=" + id + ", name=" + name + ", email=" + email + "]";
+    }
+}
