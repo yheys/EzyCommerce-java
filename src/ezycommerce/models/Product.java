57cf2d8f782aab139fcc78e419dd751562536f92
Yilkal Kebede <yheyskebede2015@gmail.com>
2026-06-16 15:55:03 +0300

    Add Product.java

---
+++ src/ezycommerce/models/Product.java
@@ -0,0 +1,48 @@
+package ezycommerce.models;
+
+public class Product {
+
+    
+    private int id;
+    private String name;
+    private double price;
+    private int stock;
+
+    
+    public Product() {
+    }
+
+    
+    public Product(int id, String name, double price, int stock) {
+        this.id    = id;
+        this.name  = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    
+    public int getId()       { return id; }
+    public String getName()  { return name; }
+    public double getPrice() { return price; }
+    public int getStock()    { return stock; }
+
+    
+    public void setId(int id)          { this.id = id; }
+    public void setName(String name)   { this.name = name; }
+    public void setPrice(double price) { this.price = price; }
+    public void setStock(int stock)    { this.stock = stock; }
+
+    
+    public void displayInfo() {
+        System.out.println("ID    : " + id);
+        System.out.println("Name  : " + name);
+        System.out.println("Price : $" + price);
+        System.out.println("Stock : " + stock);
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Product[id=" + id + ", name=" + name + ", price=$" + price + "]";
+    }
+}
